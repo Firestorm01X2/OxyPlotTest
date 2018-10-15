7d33a34bfc7a9fcdab8e85cc08586072af899873
Firestorm01X2 <firestorm01x2@gmail.com>
2018-10-15 08:39:13 +0300

    Добавил классы интеграл и факториал

---
+++ OxyPlotTest/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlotTest
+{
+   public class Factorial
+    {
+        int Calculate_Factorial(int a)
+        {
+            int result=1;
+            for (int i = 1; i < a; i++)
+                result *= a;
+            return result;
+        }
+    }
+}
